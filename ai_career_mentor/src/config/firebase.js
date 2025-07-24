aa0881bb7561b5edbe7f8879ef7b4e9e6f12a880
Yohannis <yohannisadmasu05@gmail.com>
2025-07-25 00:58:36 +0400

    authentication completed

--- ai_career_mentor/src/config/firebase.js
+++ ai_career_mentor/src/config/firebase.js
@@ -9,23 +9,20 @@ import {
     onAuthStateChanged
 } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAsv4JDONaL0jXW37r_t0MeSe69I_aOHYM",
-  authDomain: "ai-career-mentor-e2095.firebaseapp.com",
-  projectId: "ai-career-mentor-e2095",
-  storageBucket: "ai-career-mentor-e2095.appspot.com", // Corrected storage bucket name
-  messagingSenderId: "17154740601",
-  appId: "1:171547-40601:web:e3735475f8e7ecbb149fa8",
-  measurementId: "G-MVML83CVYQ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
-// Export all the necessary functions and services
 export { 
     auth, 
     createUserWithEmailAndPassword, 
